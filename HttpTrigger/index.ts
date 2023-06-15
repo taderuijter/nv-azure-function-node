da249f0d29366987495320da201e8113f4d83569
Tadeusz de Ruijter <tech@nomadventrs.com>
2023-06-15 16:56:13 +0200

    feat: added a authentication demo in the httpTrigger function

--- HttpTrigger/index.ts
+++ HttpTrigger/index.ts
@@ -1,15 +1,20 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-  context.log("HTTP trigger function processed a request.");
-  const name = req.query.name || (req.body && req.body.name);
-  const responseMessage = name
-    ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-    : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+  const userId = req.headers["x-ms-client-principal-id"];
 
+  if (!userId) {
+    context.res = {
+      status: 401,
+      body: "Unauthorized user",
+    };
+    return;
+  }
+
+  // If we reach this point, the request is authenticated.
   context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: responseMessage,
+    status: 200,
+    body: "Hello, " + userId + "!",
   };
 };
 
